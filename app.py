8b50d4a83a285deb13aed6770f534912352df145
João Henrique Silva de Miranda <joaohsm13@gmail.com>
2025-08-26 07:00:31 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ st.markdown("""
 }
 </style>
 """, unsafe_allow_html=True)
-st.title("🤖 Plataforma de Análise e Previsão de Ações")
+st.title("Plataforma de Análise e Previsão de Ações")
 
 # -----------------------------------------------------------------------------
 # CONSTS / DIRS
